6e8fcf823e2ed04e232b40d218ce43f1a4f6774a
Amin <aminmoghadas@outlook.com>
2025-09-03 18:27:39 +0330

    Initial plugin structure with PSR-4 autoload and base files

---
+++ wp-books.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: WP Books
+ * Plugin URI:  https://keywp.ir
+ * Description: WP Books plugin — PSR-4, Composer autoload, DB manager, Shortcode, AJAX handler and Admin UI.
+ * Version:     1.1.0
+ * Author:      Amin Moghadas
+ * Text Domain: wp-books
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+$autoload_path = __DIR__ . '/vendor/autoload.php';
+if ( file_exists( $autoload_path ) ) {
+    require_once $autoload_path;
+} else {
+
+}
+
